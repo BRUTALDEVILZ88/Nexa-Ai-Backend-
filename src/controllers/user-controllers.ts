64dec1ef86f92d7ee087ab5b3452a57652623c3d
Rishita Sharma <rishi88006@gmail.com>
2025-06-27 16:30:42 +0530

    Final changes

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -38,8 +38,8 @@ export const userSignup = async (
       httpOnly: true,
       path: "/",
       signed: true,
-      sameSite: "none",
-      secure: true,
+       sameSite: isProd ? "none" as "none" : "lax",
+  secure: isProd,
     });
 
     const token = createToken(user._id.toString(), user.email, "7d");
@@ -50,8 +50,8 @@ export const userSignup = async (
       path: "/",
       httpOnly: true,
       signed: true,
-      sameSite: "lax",
-      secure: true,
+       sameSite: isProd ? "none" as "none" : "lax",
+  secure: isProd,
       expires,
     });
 
@@ -79,8 +79,8 @@ export const userLogin = async (
       httpOnly: true,
       path: "/",
       signed: true,
-      sameSite: "none",
-      secure: true,
+        sameSite: isProd ? "none" as "none" : "lax",
+  secure: isProd,
     });
 
     const token = createToken(user._id.toString(), user.email, "7d");
@@ -91,8 +91,8 @@ export const userLogin = async (
       path: "/",
       httpOnly: true,
       signed: true,
-      sameSite: "none",
-      secure: true,
+      sameSite: isProd ? "none" as "none" : "lax",
+  secure: isProd,
       expires,
     });
 
@@ -140,8 +140,8 @@ export const userLogout = async (
       httpOnly: true,
       path: "/",
       signed: true,
-      sameSite: "none",
-      secure: true,
+      sameSite: isProd ? "none" as "none" : "lax",
+  secure: isProd,
     });
 
     return res.status(200).json({ message: "OK", name: user.name, email: user.email });
